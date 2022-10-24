4a907749ce6987e0a883b18b5dd5115da7cfe964
UlisesGomezDW <ulisesgomez08live@gmail.com>
2022-10-24 18:05:21 -0500

    add/elements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import { UsersComponent } from "./users/users.component"
 import { HomeComponent } from "./home/home.component"
+import { GifsComponent } from "./gifs/gifs.component"
 
 const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "basic", component: UsersComponent },
-    { path: "gifs", component: UsersComponent },
+    { path: "gifs", component: GifsComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
 
-import { AppComponent } from './app.component';
-import { UsersModule } from './users/users.module';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component"
+import { UsersModule } from "./users/users.module"
+import { AppRoutingModule } from "./app-routing.module"
+import { HomeComponent } from "./home/home.component"
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, UsersModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomeComponent],
+    imports: [BrowserModule, UsersModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
